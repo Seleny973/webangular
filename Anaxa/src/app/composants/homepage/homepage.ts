53ba1b2511ee654f547192d39030f736be1acb0d
Mervine <fredericmervine@gmail.com>
2025-10-08 15:23:37 +0000

    Sauvegarde des modifications

--- Anaxa/src/app/composants/homepage/homepage.ts
+++ Anaxa/src/app/composants/homepage/homepage.ts
@@ -1,34 +1,45 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Header } from '../header/header';
+import { ProductCard } from '../product-card/product-card';
+import { UserService } from '../../services/user-service';
 import { ProductService } from '../../services/product-service';
 
 @Component({
-    selector: 'app-homepage',
-    imports: [CommonModule, Header],
-    templateUrl: './homepage.html',
-    styleUrl: './homepage.scss'
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [ProductCard],
+  templateUrl: './homepage.html',
+  styleUrls: ['./homepage.scss']
 })
 export class Homepage {
-    // Injecte le service ProductService dans le composant
-    constructor(private productService: ProductService) {
-    // Le service est maintenant disponible pour être utilisé dans le composant
-    }
-    products: any = [];
 
-    ngOnInit(){
-        // Fait un appel à l'API pour récupérer les produits
-        // Angular attend, donc on s'abonne pour obtenir les données
-        this.productService.getProducts().subscribe({
-            //Next si il y a une réponse de l'API avec les produits
-            next: (apiResponse) => {
-                console.log('produits récupérés :', apiResponse);
-                this.products = apiResponse ;
-            },
-            //Error si il y a une erreur lors de l'appel à l'API
-            error: (error) => {
-                console.error('Erreur lors de la récupération des produits :', error);
-            }
-        });
-    }
+  constructor(
+    private userService: UserService,
+    private productService: ProductService,
+  ) {}
+  products:any = [];
+  cart:any = [];
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (apiResponse) => {
+        console.log('Produits récupérés :', apiResponse);
+        this.products = apiResponse;
+      },
+      error: (error) => {
+        
+        console.error('Erreur lors de la récupération des produits', error);
+      }
+    });
+
+    this.cart = this.productService.cart
+    console.log("cart =",this.cart);
+
+
+  }
+
+  handleProductClick(callback: any) {
+    let product = callback.event;
+    console.log(product);
+    this.cart = this.productService.addToCart(callback.product);
+    console.log("cart =",this.cart);
+  }
 }
---
+++ Anaxa/src/app/composants/login/login.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user-service';
+
+@Component({
+  selector: 'app-login',
+    imports: [CommonModule, ReactiveFormsModule],
+  standalone: true,
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private userService: UserService
+  ) {
+    this.loginForm = new FormGroup({
+      username: new FormControl('johnd', [Validators.required]),
+      password: new FormControl('m38rmF$', [Validators.required, Validators.minLength(6)]),
+    });
+  }
+
+  ngOnInit(){
+    
+    // this.userService.getAll().subscribe({
+    //   next: (response) => {
+    //     console.log(response);
+    //   },
+    //   error: (error) => {}
+    // });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+    if (this.loginForm.valid) {
+      // console.log('Formulaire valide:', this.loginForm.value);
+      this.userService.login(this.loginForm.value.username, this.loginForm.value.password)
+      .subscribe({
+        next: (response:any) => {
+          console.log('Connexion réussie', response);
+          this.userService.setToken(response.token);
+        },
+        error: (error) => {
+          console.error('Erreur lors de la connexion', error);
+        }
+      });
+      // Ici tu peux appeler un service pour créer l'utilisateur
+    } else {
+      console.log('Formulaire invalide');
+      this.loginForm.markAllAsTouched(); // affiche les erreurs
+    }
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+}
